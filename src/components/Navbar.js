f3014571ec8baeca4f92e4bc27c6fb2a04a86eb7
iashtripathy <iashkumartripathy@gmail.com>
2022-03-28 23:55:00 +0530

    logout_login_registraion_jwt done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,107 @@
 import React, { useState } from "react";
 import "./navbar.css";
-import {
-  FaFacebookSquare,
-  FaInstagramSquare,
-  FaYoutubeSquare,
-} from "react-icons/fa";
-import { GiHamburgerMenu } from "react-icons/gi";
+
 
 import { NavLink } from "react-router-dom";
 
+
+
+let isLoggedIn = false;
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+let cookie = getCookie("patient_cookie"); 
+if(cookie!=null){
+  isLoggedIn = true;
+  //alert("cookie there");
+}
+else{
+  isLoggedIn = false;
+}
+
+
+
+
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
-  return (
-    <>
-      <nav className="main-nav">
-        {/* 1st logo part  */}
-        <div className="logo">
-          <h2>
-            <span>P</span>atient
-            <span>A</span>pp
-          </h2>
-        </div>
 
-        {/* 2nd menu part  */}
-        <div
-          className={
-            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/register">Register</NavLink>
-            </li>
+  //if(isLoggedIn){
+    return (
+      <>
+        <nav className="main-nav">
+          {/* 1st logo part  */}
+          <div className="logo">
+            <h2>
+              <span>P</span>atient
+              <span>A</span>pp
+            </h2>
+          </div>
+  
+          {/* 2nd menu part  */}
+          <div
+            className={
+              showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+            }>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+              <li>
+                <NavLink to="/logout">Logout</NavLink>
+              </li>
+            </ul>
+          </div>
+  
+          
+        </nav>
+      </>
+    );
+  //}
+/*   else{
+    return (
+      <>
+        <nav className="main-nav">
+          
+          <div className="logo">
+            <h2>
+              <span>P</span>atient
+              <span>A</span>pp
+            </h2>
+          </div>
+  
+          
+          <div
+            className={
+              showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+            }>
+            <ul>
 
-          </ul>
-        </div>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
 
-        
-      </nav>
+            </ul>
+          </div>
+  
+          
+        </nav>
+      </>
+    );
+  }*/
 
-      {/* hero section  */}
-      {/* <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Thapa Technical</h1>
-      </section> */}
-    </>
-  );
 };
 
 export default Navbar;
